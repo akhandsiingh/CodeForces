7807a6724cb5b6b24c924cd40dcd9849e4b86a1c
Akhand Singh <akhandsingh4906@gmail.com>
2025-12-12 21:25:13 +0530

    Create HalloumiBoxes.java

---
+++ HalloumiBoxes.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class HalloumiBoxes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] arr = new int[n];
+            
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            if (k > 1) {
+                System.out.println("YES");
+            } else {
+                if (isSorted(arr)) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            }
+        }
+        sc.close();
+    }
+    private static boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) return false;
+        }
+        return true;
+    }
+}
