e6168a18d0757a33314c735c6a40bb8611635e49
Akhand Singh <akhandsingh4906@gmail.com>
2025-12-16 20:45:42 +0530

    Update SakurakosExam.java

--- SakurakosExam.java
+++ SakurakosExam.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-public class Main {
-    public static void SakurakosExam(String[] args) {
+public class SakurakosExam {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
 
